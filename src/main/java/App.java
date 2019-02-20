93c228d390085ec8d9c14d145f0d60d50b4288a9
Dan Orel <mail.ordan@gmail.com>
2019-02-20 06:44:14 +0200

    Heap sort: implemented the substitution of the elements, recursive showing the elements.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,8 +3,13 @@ import heap.Heap;
 public class App {
     public static void main(String[] args) {
         Heap heap = new Heap();
-        Integer []ints = {5, 13, -3, 2, 1, 0};
+        Integer []ints = {5, 13, -3, 2, 1, 0, 47};
         heap.asHeap(ints);
         heap.showHeap(heap.getRoot());
+        System.out.println();
+        heap.substituteKEY(heap.getRoot(), -3, 7);
+        heap.showHeap(heap.getRoot());
+        System.out.println();
+        System.out.println(heap.extractMAX(heap.getRoot()));
     }
 }
--- src/main/java/heap/Heap.java
+++ src/main/java/heap/Heap.java
@@ -21,38 +21,74 @@ public class Heap {
     }
 
     private void buildHeap(Integer []Array, int index, Leaf leaf){
-        if(2 * index < Array.length){
-            leaf.leftLeaf = new Leaf(Array[(2 * index + 1) - 1]);
-            buildHeap(Array, 2 * index + 1, leaf.leftLeaf);
-            leaf.rightLeaf = new Leaf(Array[(2 * index - 1) ]);
-            buildHeap(Array, 2 * index , leaf.rightLeaf);
+        if(2 * index - 1 < Array.length){
+            leaf.leftLeaf = new Leaf(Array[(2 * index - 1) ]);
+            buildHeap(Array, 2 * index , leaf.leftLeaf);
+        }
+        if(2 * index < Array.length) {
+            leaf.rightLeaf = new Leaf(Array[2 * index]);
+            buildHeap(Array, 2 * index + 1, leaf.rightLeaf);
         }
     }
 
+    public Heap insertKEY(Leaf root, Integer key){
 
-//    public Heap insertKEY(S, x){
-//
-//        return this;
-//    }
-//
-//    public Heap extractMAX(S){
-//
-//        return this;
-//    }
-//
-//    public Heap substituteKEY(S, x, s){
-//
-//        return this;
-//    }
+        return this;
+    }
 
-    public void showHeap(Leaf leaf){
-        System.out.println("-" + leaf.key);
-        while(leaf.leftLeaf != null || leaf.rightLeaf != null){
+    public int extractMAX(Leaf root){
+        return extractMAX(root, root.key);
+    }
+
+    private int extractMAX(Leaf leaf, Integer MAX){
+        if(leaf != null){
+            if(leaf.key >= MAX) MAX = leaf.key;
             if(leaf.leftLeaf != null){
-                showHeap(leaf.leftLeaf);
+                return extractMAX(leaf.leftLeaf, MAX);
             }
             if(leaf.rightLeaf != null){
-                showHeap(leaf.rightLeaf);
+                return extractMAX(leaf.rightLeaf, MAX);
+            }
+        }
+        return MAX;
+    }
+
+    public Heap substituteKEY(Leaf root, Integer key, Integer value){
+        substitute(root, key, value);
+        return this;
+    }
+
+    private void substitute(Leaf leaf, Integer key, Integer value){
+        if(leaf != null){
+            if(leaf.key.equals(key)){
+                leaf.key = value;
+            }
+            if(leaf.leftLeaf != null){
+                substitute(leaf.leftLeaf, key, value);
+            }
+            if(leaf.rightLeaf != null){
+                substitute(leaf.rightLeaf, key, value);
+            }
+        }
+    }
+
+    public void showHeap(Leaf leaf){
+        showHeap(leaf, 1);
+    }
+
+    private void showHeap(Leaf leaf, int layer){
+        if(leaf != null){
+            for(int times = 0; times < layer; times++){
+                System.out.print("-");
+            }
+            System.out.println("(" + leaf.key + ")");
+            if(leaf.rightLeaf != null || leaf.leftLeaf != null){
+                if(leaf.leftLeaf != null){
+                    showHeap(leaf.leftLeaf, layer + 1);
+                }
+                if(leaf.rightLeaf != null){
+                    showHeap(leaf.rightLeaf, layer + 1);
+                }
             }
         }
     }
@@ -63,8 +99,8 @@ public class Heap {
 
     private class Leaf {
         private Integer key;
-        public Leaf leftLeaf = null;
         public Leaf rightLeaf = null;
+        public Leaf leftLeaf = null;
 
         public Leaf(Integer key){
             this.key = key;
